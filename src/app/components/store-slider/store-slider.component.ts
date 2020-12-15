793bd3f3cbbae121dd895b5dfcff4a3af053c7d8
Enmanuel Balcacer <2koders@gmail.com>
2020-12-15 13:58:34 -0400

    StoreSlider ready

--- src/app/components/store-slider/store-slider.component.ts
+++ src/app/components/store-slider/store-slider.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreSliderComponent implements OnInit {
 
+  images = [944, 1021, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
